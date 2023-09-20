4f0972e7c9c2a5aa77029ac1dee094847df487ea
standik3 <standika3@gmail.com>
2023-09-20 13:18:12 +0700

    update

--- android/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
 import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val vueApp = findViewById<WebView>(R.id.myVueApp)
+        val settings: WebSettings = vueApp.getSettings()
+        settings.domStorageEnabled = true
+        settings.allowFileAccess = true
+        settings.databaseEnabled = true
         vueApp.settings.javaScriptEnabled = true
 //        vueApp.loadUrl("https://detik.com/")
         vueApp.loadUrl("https://botinformasi.netlify.app/")
