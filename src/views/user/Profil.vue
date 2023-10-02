dde52c8260b7f6ce8755ef8bc9db3420eef8dc4c
standik3 <standika3@gmail.com>
2023-10-02 12:05:59 +0700

    update

--- src/views/user/Profil.vue
+++ src/views/user/Profil.vue
@@ -7,9 +7,9 @@
     </div>
     <div class="flex flex-col flex-grow items-center justify-center">
         <h1 class="text-4xl font-bold text-gray-800 px-4 pb-5">Profil</h1>
-        <img class="object-cover w-50 h-50 rounded-full" src="https://via.placeholder.com/100x100.png/007BFF/FFFFFF/?text=${photoURL}" alt="photoURL" />
+        <img class="object-cover w-50 h-50 rounded-full" :src="photoURL" :alt="photoURL" />
         <p class="text-gray-500">NAMA    : {{ nama }}<br>EMAIL   : {{ email }} </p>
-        <p class="text-gray-500">{{ photoURL }} {{ bio }}</p>
+        <p class="text-gray-500">{{ bio }}</p>
         <div class="relative flex justify-between">
             <button class="bg-green-500 hover:bg-green-700 text-white font-bold mx-2 my-2 py-2 px-4 rounded-full" @click="editProfil">Edit Profil</button>
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold mx-2 my-2 py-2 px-4 rounded-full" @click="handleLogout">Logout</button>
@@ -40,12 +40,12 @@ export default {
         },
         loadUser() {
             let user = JSON.parse(localStorage.getItem('user'));
-
+            let photoURL = "https://via.placeholder.com/100x100.png/007BFF/FFFFFF/?text=" + user.photo
             this.uid = user.uid;
             this.bio = user.bio;
             this.nama = user.name;
             this.email = user.email;
-            this.photoURL = user.photo;
+            this.photoURL = photoURL;
         },
         handleLogout() {
             signOut(auth).then(() => {
