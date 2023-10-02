7cda82a6ed482e8bf5cb2f6dd3217329274a26d1
standik3 <standika3@gmail.com>
2023-10-02 10:18:25 +0700

    update

--- src/views/user/Profil.vue
+++ src/views/user/Profil.vue
@@ -7,9 +7,9 @@
     </div>
     <div class="flex flex-col flex-grow items-center justify-center">
         <h1 class="text-4xl font-bold text-gray-800 px-4 pb-5">Profil</h1>
-        <img class="object-cover w-50 h-50 rounded-full" :src="photoURL" />
-        <p class="text-gray-500">NAMA :{{ nama }}</p>
-        <p class="text-gray-500">EMAIL:{{ email }}</p>
+        <img class="object-cover w-50 h-50 rounded-full" :src="photoURL" :alt="nama" />
+        <p class="text-gray-500">NAMA :{{ nama }} <br>EMAIL:{{ email }} </p>
+        <!-- <p class="text-gray-500"></p> -->
         <p class="text-gray-500">BIO  :{{ bio }}</p>
         <div class="relative flex justify-between">
             <button class="bg-green-500 hover:bg-green-700 text-white font-bold mx-2 my-2 py-2 px-4 rounded-full" @click="editProfil">Edit Profil</button>
