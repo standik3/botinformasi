a0f71f3dfa56700582ad00da9ff2f9d37eabec3a
standik3 <standika3@gmail.com>
2023-11-23 03:39:11 +0700

    update

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -4,16 +4,16 @@
             class="flex flex-col flex-grow items-center justify-center w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
             <!-- begin:: body -->
             <div class="bg-white lg:w-6/12 md:7/12 w-8/12 shadow-3xl">
-                <div
+                <!-- <div
                     class="bg-gray-800 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
                     <svg width="32" height="32" viewBox="0 0 24 24" fill="#FFF">
                         <path
                             d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z" />
                     </svg>
-                </div>
+                </div> -->
                 <div class="md:pt-24">
                     <div class="flex items-center text-lg mb-6 md:mb-8">
-                        <!-- <font-awesome-icon class="absolute ml-3" icon="fa-solid fa-user" /> -->
+                        <font-awesome-icon class="absolute ml-3" icon="fa-solid fa-user" />
                         <input type="text" id="name" ref="name" :placeholder="name" required class="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" />
                     </div>
                     <div class="flex items-center text-lg mb-6 md:mb-8">
