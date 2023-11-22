03e308f319cc40f22fc42935ff7105fadc8da418
standik3 <standika3@gmail.com>
2023-11-23 03:29:13 +0700

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,7 +9,6 @@
                 </svg>
             </div>
             <div class="p-12 md:p-24">
-                <font-awesome-icon class="absolute ml-3" icon="fa-solid fa-envelope" />
                 <div class="flex items-center text-lg mb-6 md:mb-8">
                     <input type="text" id="email" ref="email" :placeholder="email"
                         class="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" />
