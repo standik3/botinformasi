912c657150c1271b923b506bcb6b552f66db83d6
standik3 <standika3@gmail.com>
2023-12-21 15:38:20 +0700

    update

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -65,9 +65,9 @@ import Breadcrumb from "../../components/admin/Breadcrumb.vue";
         </table>
         <!-- Pagination controls -->
         <div>
-            <button class="bg-green-500 btn-sm hover:bg-green-700 text-white font-medium px-3 py-2 rounded-lg" @click="prevPage" :disabled="currentPage === 1">Previous Page</button>
+            <button class="bg-red-500 btn-sm hover:bg-red-700 text-white font-medium px-3 py-2 rounded-lg" @click="prevPage" :disabled="currentPage === 1">Previous Page</button>
             <span>{{ currentPage }} of {{ totalPages }}</span>
-            <button @click="nextPage" :disabled="currentPage === totalPages"> Next</button>
+            <button class="bg-green-500 btn-sm hover:bg-green-700 text-white font-medium px-3 py-2 rounded-lg" @click="nextPage" :disabled="currentPage === totalPages"> Next</button>
         </div>
     </div>
 </template>
